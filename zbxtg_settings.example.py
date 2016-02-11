6d6e249a7a1b19a2115be5580f75965202d330df
TheMikeBonkers <Melnikov1987@gmail.com>
2016-02-11 15:52:16 +0400

    Update zbxtg_settings.example.py

--- zbxtg_settings.example.py
+++ zbxtg_settings.example.py
@@ -1,10 +1,10 @@
-tg_key = "XYZ"  # telegram bot api key
+tg_key = "172303828:AAFhXQvN4IRKIasgz_lTLtRkLRtVGDLTxfc"  # telegram bot api key
 
 zbx_tg_prefix = "zbxtg"  # variable for separating text from script info
 zbx_tg_tmp_dir = "/tmp/" + zbx_tg_prefix  # directory for saving caches, uids, cookies, etc.
 zbx_tg_signature = False
 
-zbx_server = "http://localhost"  # zabbix server full url
+zbx_server = "http://172.20.69.226"  # zabbix server full url
 zbx_api_user = "api"
 zbx_api_pass = "api"
 zbx_api_verify = True  # True - do not ignore self signed certificates, False - ignore
